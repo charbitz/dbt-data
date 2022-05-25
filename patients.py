8f4653244dc0b72b488c3e81856ad5b68a44304b
charbitz <charbitz20@gmail.com>
2022-05-25 21:16:13 +0300

    Some information from prints was processed.

--- patients.py
+++ patients.py
@@ -5,22 +5,25 @@ import numpy as np
 df_labels = pd.read_csv("/mnt/seagate/DBT/duke-dbt-data_github/data/manifest-1617905855234/BCS-DBT labels-new-v0.csv")
 
 # Printing to check the dataframe:
-print(df_labels.head())
-print()
-print("df shape:", df_labels.shape)
-print(df_labels.columns.tolist())
+# print(df_labels.head())
+# print()
+# print("df shape:", df_labels.shape)
+# print(df_labels.columns.tolist())
 
+# ~ ~ ~ Biopsied group: ~ ~ ~
 # Creating a set for all Patients in the biopsied :{"Benign", "Cancer"} groups:
 set_biopsied = set(df_labels[(df_labels["Benign"] == 1) | (df_labels["Cancer"] == 1)]["PatientID"])
-print("Number of patients at the biopsied groups:", len(set_biopsied))
+print("Number of patients at the Biopsied groups: {Benign, Cancer}", len(set_biopsied))
 
+# ~ ~ ~ Normal group: ~ ~ ~
 # Creating a set for all patients in the "Normal" group:
 set_normal = set(df_labels[df_labels["Normal"] == 1]["PatientID"])
-print("Number of patients at the 'Normal' group:", len(set_normal))
+print("Number of patients at the Normal group:", len(set_normal))
 
 # Creating a set for all patients in two previously sets to check if there are common parts:
 set_final = set_biopsied.union(set_normal)
 print("Number of patients at all 3 groups:", len(set_final))
+print()
 
 # set_final = sorted(set_final)
 #
@@ -31,14 +34,11 @@ print("Number of patients at all 3 groups:", len(set_final))
 # # Printing again:
 # print(set_final)
 
-
-
 set_triplets_norm = set()
 list_triplets_norm = []
 
 set_triplets_bio = set()
 list_triplets_bio = []
-
 
 for iter in range(df_labels.shape[0]):
   elem = str(df_labels.loc[iter]["PatientID"]) + str(df_labels.loc[iter]["StudyUID"]) + str(df_labels.loc[iter]["View"])
@@ -49,11 +49,8 @@ for iter in range(df_labels.shape[0]):
   set_triplets_bio.add(elem)
   list_triplets_bio.append(elem)
 
-
-
 print("Number of normal volumes at PatientID/StudyUID/View in a list:", len(list_triplets_norm))
 print("Number of normal volumes at PatientID/StudyUID/View in a set:", len(set_triplets_norm))
- 
 
 print("Number of all biopsied and normal volumes at PatientID/StudyUID/View in a list:", len(list_triplets_bio))
 print("Number of all biopsied and normal volumes at PatientID/StudyUID/View in a set:", len(set_triplets_bio))
