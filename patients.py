e8e15ee93933daadcb4c0762b8eacaa7522caced
charbitz <charbitz20@gmail.com>
2022-05-25 18:37:34 +0300

    Change the df name.

--- patients.py
+++ patients.py
@@ -2,20 +2,20 @@ import pandas as pd
 import numpy as np
 
 # Reading the csv file with category labels, containing both the biopsied volumes (200) and the normal ones (+200): 
-df = pd.read_csv("/mnt/seagate/DBT/duke-dbt-data_github/data/manifest-1617905855234/BCS-DBT labels-new-v0.csv")
+df_labels = pd.read_csv("/mnt/seagate/DBT/duke-dbt-data_github/data/manifest-1617905855234/BCS-DBT labels-new-v0.csv")
 
 # Printing to check the dataframe:
-print(df.head())
+print(df_labels.head())
 print()
-print("df shape:", df.shape)
-print(df.columns.tolist())
+print("df shape:", df_labels.shape)
+print(df_labels.columns.tolist())
 
 # Creating a set for all Patients in the biopsied :{"Benign", "Cancer"} groups:
-set_biopsied = set(df[(df["Benign"] == 1) | (df["Cancer"] == 1)]["PatientID"])
+set_biopsied = set(df_labels[(df_labels["Benign"] == 1) | (df_labels["Cancer"] == 1)]["PatientID"])
 print("Number of patients at the biopsied groups:", len(set_biopsied))
 
 # Creating a set for all patients in the "Normal" group:
-set_normal = set(df[df["Normal"] == 1]["PatientID"])
+set_normal = set(df_labels[df_labels["Normal"] == 1]["PatientID"])
 print("Number of patients at the 'Normal' group:", len(set_normal))
 
 # Creating a set for all patients in two previously sets to check if there are common parts:
@@ -40,8 +40,8 @@ set_triplets_bio = set()
 list_triplets_bio = []
 
 
-for iter in range(df.shape[0]):
-  elem = str(df.loc[iter]["PatientID"]) + str(df.loc[iter]["StudyUID"]) + str(df.loc[iter]["View"])
+for iter in range(df_labels.shape[0]):
+  elem = str(df_labels.loc[iter]["PatientID"]) + str(df_labels.loc[iter]["StudyUID"]) + str(df_labels.loc[iter]["View"])
   if iter >= 200:
     # print("elem:", elem)
     set_triplets_norm.add(elem)
